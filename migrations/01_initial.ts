d920b408ca3970f036c5836d6a9b77ac52b4c99d
Ben <ben.armstrong22@gmail.com>
2022-12-02 21:34:58 +0000

    Update migrations script to work with .env file names

--- migrations/01_initial.ts
+++ migrations/01_initial.ts
@@ -4,8 +4,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('user')
     .addColumn('id', 'integer', (col) => col.autoIncrement().primaryKey())
-    .addColumn('first_name', 'varchar(255)', (col) => col.notNull())
-    .addColumn('last_name', 'varchar(255)', (col) => col.notNull())
+    .addColumn('name', 'varchar(255)', (col) => col.notNull())
     .addColumn('password', 'varchar(255)', (col) => col.notNull())
     .addColumn('email', 'varchar(255)', (col) => col.notNull().unique())
     .addColumn('is_email_verified', 'boolean', (col) => col.defaultTo(false))
