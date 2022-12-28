92582cb596b6ed0c1783b33341e46df07266a543
Ben <ben.armstrong22@gmail.com>
2022-12-28 15:24:40 +0000

    Test 404s

---
+++ tests/integration/index.test.ts
@@ -0,0 +1,11 @@
+import httpStatus from 'http-status'
+import { request } from '../utils/testRequest'
+
+describe('Basic routing', () => {
+  test('should return 404 if route not found', async () => {
+    const res = await request('/idontexist', {
+      method: 'GET',
+    })
+    expect(res.status).toBe(httpStatus.NOT_FOUND)
+  })
+})
