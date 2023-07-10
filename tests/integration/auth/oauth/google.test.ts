4cfd576e0bb6c923fd058d13b123417caf5dcc86
Ben <ben.armstrong22@gmail.com>
2023-07-10 22:58:19 +0700

    Fix test indentation

--- tests/integration/auth/oauth/google.test.ts
+++ tests/integration/auth/oauth/google.test.ts
@@ -37,182 +37,181 @@ describe('Oauth Google routes', () => {
         'response_type=code&scope=openid%20email%20profile&state=pass-through%20value'
       )
     })
+  })
+  describe('POST /v1/auth/google/callback', () => {
+    let newUser: Omit<OauthUser, 'providerType'>
+    beforeAll(async () => {
+      newUser = {
+        id: faker.datatype.number(),
+        name: faker.name.fullName(),
+        email: faker.internet.email(),
+      }
+    })
+    test('should return 200 and successfully register user if request data is ok', async () => {
+      const fetchMock = getMiniflareFetchMock()
+      const googleApiMock = fetchMock.get('https://www.googleapis.com')
+      googleApiMock
+        .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
+        .reply(200, JSON.stringify(newUser))
+      const googleMock = fetchMock.get('https://oauth2.googleapis.com')
+      googleMock
+        .intercept({method: 'POST', path: '/token'})
+        .reply(200, JSON.stringify({access_token: '1234'}))
 
-    describe('POST /v1/auth/google/callback', () => {
-      let newUser: Omit<OauthUser, 'providerType'>
-      beforeAll(async () => {
-        newUser = {
-          id: faker.datatype.number(),
-          name: faker.name.fullName(),
-          email: faker.internet.email(),
+      const providerId = '123456'
+      const res = await request('/v1/auth/google/callback', {
+        method: 'POST',
+        body: JSON.stringify({code: providerId}),
+        headers: {
+          'Content-Type': 'application/json'
         }
       })
-      test('should return 200 and successfully register user if request data is ok', async () => {
-        const fetchMock = getMiniflareFetchMock()
-        const googleApiMock = fetchMock.get('https://www.googleapis.com')
-        googleApiMock
-          .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
-          .reply(200, JSON.stringify(newUser))
-        const googleMock = fetchMock.get('https://oauth2.googleapis.com')
-        googleMock
-          .intercept({method: 'POST', path: '/token'})
-          .reply(200, JSON.stringify({access_token: '1234'}))
-
-        const providerId = '123456'
-        const res = await request('/v1/auth/google/callback', {
-          method: 'POST',
-          body: JSON.stringify({code: providerId}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
-        expect(res.status).toBe(httpStatus.OK)
-        expect(body.user).not.toHaveProperty('password')
-        expect(body.user).toEqual({
-          id: expect.anything(),
-          name: newUser.name,
-          email: newUser.email,
-          role: 'user',
-          is_email_verified: 1
-        })
-
-        const dbUser = await client
-          .selectFrom('user')
-          .selectAll()
-          .where('user.id', '=', body.user.id)
-          .executeTakeFirst()
-
-        expect(dbUser).toBeDefined()
-        if (!dbUser) return
-
-        expect(dbUser.password).toBeNull()
-        expect(dbUser).toMatchObject({
-          name: newUser.name,
-          password: null,
-          email: newUser.email,
-          role: 'user',
-          is_email_verified: 1
-        })
-
-        const oauthUser = await client
-          .selectFrom('authorisations')
-          .selectAll()
-          .where('authorisations.provider_type', '=', authProviders.GOOGLE)
-          .where('authorisations.user_id', '=', body.user.id)
-          .where('authorisations.provider_user_id', '=', String(newUser.id))
-          .executeTakeFirst()
-
-        expect(oauthUser).toBeDefined()
-        if (!oauthUser) return
-
-        expect(body.tokens).toEqual({
-          access: { token: expect.anything(), expires: expect.anything() },
-          refresh: { token: expect.anything(), expires: expect.anything() }
-        })
+      const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
+      expect(res.status).toBe(httpStatus.OK)
+      expect(body.user).not.toHaveProperty('password')
+      expect(body.user).toEqual({
+        id: expect.anything(),
+        name: newUser.name,
+        email: newUser.email,
+        role: 'user',
+        is_email_verified: 1
       })
 
-      test('should return 200 and successfully login user if already created', async () => {
-        const ids = await insertUsers([userOne], config.database)
-        const userId = ids[0]
-        const googleUser = googleAuthorisation(userId)
-        await insertAuthorisations([googleUser], config.database)
-        newUser.id = parseInt(googleUser.provider_user_id)
+      const dbUser = await client
+        .selectFrom('user')
+        .selectAll()
+        .where('user.id', '=', body.user.id)
+        .executeTakeFirst()
 
-        const fetchMock = getMiniflareFetchMock()
-        const googleApiMock = fetchMock.get('https://www.googleapis.com')
-        googleApiMock
-          .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
-          .reply(200, JSON.stringify(newUser))
-        const googleMock = fetchMock.get('https://oauth2.googleapis.com')
-        googleMock
-          .intercept({method: 'POST', path: '/token'})
-          .reply(200, JSON.stringify({access_token: '1234'}))
+      expect(dbUser).toBeDefined()
+      if (!dbUser) return
 
-        const providerId = '123456'
-        const res = await request('/v1/auth/google/callback', {
-          method: 'POST',
-          body: JSON.stringify({code: providerId}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
-        expect(res.status).toBe(httpStatus.OK)
-        expect(body.user).not.toHaveProperty('password')
-        expect(body.user).toEqual({
-          id: userId,
-          name: userOne.name,
-          email: userOne.email,
-          role: userOne.role,
-          is_email_verified: 0
-        })
-
-        expect(body.tokens).toEqual({
-          access: { token: expect.anything(), expires: expect.anything() },
-          refresh: { token: expect.anything(), expires: expect.anything() }
-        })
+      expect(dbUser.password).toBeNull()
+      expect(dbUser).toMatchObject({
+        name: newUser.name,
+        password: null,
+        email: newUser.email,
+        role: 'user',
+        is_email_verified: 1
       })
 
-      test('should return 403 if user exists but has not linked their google', async () => {
-        await insertUsers([userOne], config.database)
-        newUser.email = userOne.email
+      const oauthUser = await client
+        .selectFrom('authorisations')
+        .selectAll()
+        .where('authorisations.provider_type', '=', authProviders.GOOGLE)
+        .where('authorisations.user_id', '=', body.user.id)
+        .where('authorisations.provider_user_id', '=', String(newUser.id))
+        .executeTakeFirst()
 
-        const fetchMock = getMiniflareFetchMock()
-        const googleApiMock = fetchMock.get('https://www.googleapis.com')
-        googleApiMock
-          .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
-          .reply(200, JSON.stringify(newUser))
-        const googleMock = fetchMock.get('https://oauth2.googleapis.com')
-        googleMock
-          .intercept({method: 'POST', path: '/token'})
-          .reply(200, JSON.stringify({access_token: '1234'}))
+      expect(oauthUser).toBeDefined()
+      if (!oauthUser) return
 
-        const providerId = '123456'
-        const res = await request('/v1/auth/google/callback', {
-          method: 'POST',
-          body: JSON.stringify({code: providerId}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
-        expect(res.status).toBe(httpStatus.FORBIDDEN)
-        expect(body).toEqual({
-          code: httpStatus.FORBIDDEN,
-          message: 'Cannot signup with google, user already exists with that email'
-        })
+      expect(body.tokens).toEqual({
+        access: { token: expect.anything(), expires: expect.anything() },
+        refresh: { token: expect.anything(), expires: expect.anything() }
+      })
+    })
+
+    test('should return 200 and successfully login user if already created', async () => {
+      const ids = await insertUsers([userOne], config.database)
+      const userId = ids[0]
+      const googleUser = googleAuthorisation(userId)
+      await insertAuthorisations([googleUser], config.database)
+      newUser.id = parseInt(googleUser.provider_user_id)
+
+      const fetchMock = getMiniflareFetchMock()
+      const googleApiMock = fetchMock.get('https://www.googleapis.com')
+      googleApiMock
+        .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
+        .reply(200, JSON.stringify(newUser))
+      const googleMock = fetchMock.get('https://oauth2.googleapis.com')
+      googleMock
+        .intercept({method: 'POST', path: '/token'})
+        .reply(200, JSON.stringify({access_token: '1234'}))
+
+      const providerId = '123456'
+      const res = await request('/v1/auth/google/callback', {
+        method: 'POST',
+        body: JSON.stringify({code: providerId}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
+      expect(res.status).toBe(httpStatus.OK)
+      expect(body.user).not.toHaveProperty('password')
+      expect(body.user).toEqual({
+        id: userId,
+        name: userOne.name,
+        email: userOne.email,
+        role: userOne.role,
+        is_email_verified: 0
       })
 
-
-      test('should return 401 if code is invalid', async () => {
-        const fetchMock = getMiniflareFetchMock()
-        const googleMock = fetchMock.get('https://oauth2.googleapis.com')
-        googleMock
-          .intercept({method: 'POST', path: '/token'})
-          .reply(httpStatus.UNAUTHORIZED, JSON.stringify({error: 'error'}))
-
-        const providerId = '123456'
-        const res = await request('/v1/auth/google/callback', {
-          method: 'POST',
-          body: JSON.stringify({code: providerId}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        expect(res.status).toBe(httpStatus.UNAUTHORIZED)
+      expect(body.tokens).toEqual({
+        access: { token: expect.anything(), expires: expect.anything() },
+        refresh: { token: expect.anything(), expires: expect.anything() }
       })
+    })
 
-      test('should return 400 if no code provided', async () => {
-        const res = await request('/v1/auth/google/callback', {
-          method: 'POST',
-          body: JSON.stringify({}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        expect(res.status).toBe(httpStatus.BAD_REQUEST)
+    test('should return 403 if user exists but has not linked their google', async () => {
+      await insertUsers([userOne], config.database)
+      newUser.email = userOne.email
+
+      const fetchMock = getMiniflareFetchMock()
+      const googleApiMock = fetchMock.get('https://www.googleapis.com')
+      googleApiMock
+        .intercept({method: 'GET', path: '/oauth2/v2/userinfo'})
+        .reply(200, JSON.stringify(newUser))
+      const googleMock = fetchMock.get('https://oauth2.googleapis.com')
+      googleMock
+        .intercept({method: 'POST', path: '/token'})
+        .reply(200, JSON.stringify({access_token: '1234'}))
+
+      const providerId = '123456'
+      const res = await request('/v1/auth/google/callback', {
+        method: 'POST',
+        body: JSON.stringify({code: providerId}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
+      const body = await res.json<{ user: UserResponse; tokens: TokenResponse }>()
+      expect(res.status).toBe(httpStatus.FORBIDDEN)
+      expect(body).toEqual({
+        code: httpStatus.FORBIDDEN,
+        message: 'Cannot signup with google, user already exists with that email'
+      })
+    })
+
+
+    test('should return 401 if code is invalid', async () => {
+      const fetchMock = getMiniflareFetchMock()
+      const googleMock = fetchMock.get('https://oauth2.googleapis.com')
+      googleMock
+        .intercept({method: 'POST', path: '/token'})
+        .reply(httpStatus.UNAUTHORIZED, JSON.stringify({error: 'error'}))
+
+      const providerId = '123456'
+      const res = await request('/v1/auth/google/callback', {
+        method: 'POST',
+        body: JSON.stringify({code: providerId}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      expect(res.status).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    test('should return 400 if no code provided', async () => {
+      const res = await request('/v1/auth/google/callback', {
+        method: 'POST',
+        body: JSON.stringify({}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      expect(res.status).toBe(httpStatus.BAD_REQUEST)
     })
   })
   describe('POST /v1/auth/google/:userId', () => {
