59f70ceb2d82dd0fb1873733b818190b9ba66494
Fergus Armstrong <fergus.armstrong1@gmail.com>
2024-08-24 17:26:28 +0100

    Linted

--- tests/mocks/awsClientStub/expectMock.ts
+++ tests/mocks/awsClientStub/expectMock.ts
@@ -1,27 +1,30 @@
-import { AwsStub } from "./awsClientStub";
+import { AwsStub } from './awsClientStub'
 
 // eslint-disable-next-line
 export function toHaveReceivedCommandTimes(mock: AwsStub<any, any, any>, command: unknown, times: number) {
-	const calls = mock.send.mock.calls
+  const calls = mock.send.mock.calls
+    // eslint-disable-next-line
 		.filter((call) => call[0] instanceof (command as any))
 		.length
 
-	return {
-		pass: calls === times,
-		message: () => `Function was called ${calls} times with input, expected ${times} calls`
-	}
+  return {
+    pass: calls === times,
+    message: () => `Function was called ${calls} times with input, expected ${times} calls`
+  }
 }
 
 export const expectExtension = {
-	toHaveReceivedCommandTimes
+  toHaveReceivedCommandTimes
 }
 
 interface CustomMatchers<R = unknown> {
-	// eslint-disable-next-line
+  // eslint-disable-next-line
 	toHaveReceivedCommandTimes: (command: unknown, times: number) => R
 }
 
-declare module "vitest" {
+declare module 'vitest' {
+  // eslint-disable-next-line
 	interface Assertion<T = any> extends CustomMatchers<T> {}
+  // eslint-disable-next-line
 	interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
